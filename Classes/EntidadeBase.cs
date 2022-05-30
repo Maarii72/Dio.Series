25448c2e2bb04c769938fc6ed13cf5608422e040
Marilena Sales <marilena.sales@gmail.com>
2022-05-30 16:21:15 -0300

    entidade base

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,8 @@
+namespace Dio.Series
+{
+    public abstract class EntidadeBase
+    {
+        //o Id disponível para todos que herdarem a class
+        public int Id {get; protected set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Dio.Series
+{
+  class Program
+  {
+    static void Main(string[] args){
+      Console.WriteLine("olá mundo");
+    }
+  }
+}
+
